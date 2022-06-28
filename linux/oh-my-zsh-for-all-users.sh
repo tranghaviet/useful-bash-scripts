851ff77d2d320031383795ed9a5b2fbde6f3a1df
Trang Ha Viet <viettrangha@gmail.com>
2022-06-28 22:38:17 +0700

    fix conflict

--- linux/oh-my-zsh-for-all-users.sh
+++ linux/oh-my-zsh-for-all-users.sh
@@ -4,27 +4,26 @@ echo "Install oh-my-zsh for all users"
 echo "This script should install as sudoer user but not root"
 
 echo "Installing zsh & oh-my-zsh"
-apt install zsh
-cd ~
+sudo apt install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chown -R root:root .oh-my-zsh .zshrc
-chmod -R 755 .oh-my-zsh
-chmod 744 .zshrc
+sudo chown -R root:root .oh-my-zsh .zshrc
+sudo chmod -R 755 .oh-my-zsh
+sudo chmod 744 .zshrc
 
 echo "Config .zshrc"
-echo "Auto load .bash_profile"
-echo "if [ -f ~/.bash_profile ]; then
-    . ~/.bash_profile;
-fi" >> ~/.zshrc
+# echo "Auto load .bash_profile"
+# echo "if [ -f ~/.bash_profile ]; then
+#     . ~/.bash_profile;
+# fi" >> ~/.zshrc
 
-echo 'HIST_STAMPS="dd.mm.yyyy"' >> ~/.zshrc
-echo "HISTSIZE=10000" >> ~/.zshrc
+echo 'HIST_STAMPS="dd.mm.yyyy"' | sudo tee -a ~/.zshrc
+echo "HISTSIZE=10000"  | sudo tee -a ~/.zshrc
 
-echo "NOW=$(date +%Y-%m-%d_%H-%M-%S)" >> ~/.zshrc
-echo "DATE=$(date +%Y-%m-%d)" >> ~/.zshrc
+echo "NOW=$(date +%Y-%m-%d_%H-%M-%S)" | sudo tee -a ~/.zshrc
+echo "DATE=$(date +%Y-%m-%d)" | sudo tee -a ~/.zshrc
 
 echo "switch $USER & root terminal to zsh"
-usermod --shell $(which zsh) $USER
-usermod --shell $(which zsh) root
-ln -s /home/$USER/.oh-my-zsh /root/
-ln -s /home/$USER/.zshrc /root/
+sudo usermod --shell $(which zsh) $USER
+sudo usermod --shell $(which zsh) root
+sudo ln -s /home/$USER/.oh-my-zsh /root/
+sudo ln -s /home/$USER/.zshrc /root/
