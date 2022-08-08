44513f9be0b00dc54291d08272411481180bd80f
Trang Ha Viet <viettrangha@gmail.com>
2022-08-08 17:30:24 +0700

    update zsh script

--- linux/oh-my-zsh-for-all-users.sh
+++ linux/oh-my-zsh-for-all-users.sh
@@ -4,7 +4,7 @@ echo "Install oh-my-zsh for all users"
 echo "This script should install as sudoer user but not root"
 
 echo "Installing zsh & oh-my-zsh"
-sudo apt install zsh
+sudo apt install zsh -y
 sudo usermod --shell $(which zsh) $USER
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo chown -R root:root .oh-my-zsh .zshrc
@@ -16,6 +16,9 @@ echo "Config .zshrc"
 # echo "if [ -f ~/.bash_profile ]; then
 #     . ~/.bash_profile;
 # fi" >> ~/.zshrc
+sudo sed -i "s/robbyrussell/fletcherm/" ~/.zshrc
+sudo sed -i 's/# DISABLE_AUTO_TITLE="true"/DISABLE_AUTO_TITLE="true"/' ~/.zshrc
+sudo sed -i 's/# ENABLE_CORRECTION="true"/ENABLE_CORRECTION="true"/' ~/.zshrc
 
 echo 'HIST_STAMPS="dd.mm.yyyy"' | sudo tee -a ~/.zshrc
 echo "HISTSIZE=10000"  | sudo tee -a ~/.zshrc
@@ -27,3 +30,5 @@ echo "switch $USER & root terminal to zsh"
 sudo usermod --shell $(which zsh) root
 sudo ln -s /home/$USER/.oh-my-zsh /root/
 sudo ln -s /home/$USER/.zshrc /root/
+
+source ~/.zshrc
