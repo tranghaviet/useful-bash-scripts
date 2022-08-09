22654b6ff3d05b9a69f9ba60743d8d01fda04054
Trang Ha Viet <viettrangha@gmail.com>
2022-08-09 16:23:24 +0700

    update create user script

--- linux/create-user-with-existing-oh-my-zsh.sh
+++ linux/create-user-with-existing-oh-my-zsh.sh
@@ -6,13 +6,15 @@ echo "Username to create:"
 read username
 
 echo "Username have oh-my-zsh:"
-read ohmyzsh_user
+read targer_user
 
 useradd -m -s $(which zsh) $username
 
+TARGET_USER_HOME=$(eval echo ~$targer_user)
+
 sudo -i -u $username bash << EOF
-ln -s /home/$ohmyzsh_user/.oh-my-zsh /home/$username
-ln -s /home/$ohmyzsh_user/.zshrc /home/$username
+ln -s $TARGET_USER_HOME/.oh-my-zsh /home/$username
+ln -s $TARGET_USER_HOME/.zshrc /home/$username
 EOF
 
 echo "Add user to sudo group? (y/n)"
