c78d9f1dbd0d1badd31741ea1a2d44bb25ada49b
Trang Ha Viet <viettrangha@gmail.com>
2022-08-11 15:14:54 +0700

    update install oh-my-zsh script

--- linux/oh-my-zsh-for-all-users.sh
+++ linux/oh-my-zsh-for-all-users.sh
@@ -6,7 +6,7 @@ echo "This script should install as sudoer user but not root"
 echo "Installing zsh & oh-my-zsh"
 sudo apt install zsh -y
 sudo usermod --shell $(which zsh) $USER
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" "" --unattended
 sudo chown -R root:root .oh-my-zsh .zshrc
 sudo chmod -R 755 .oh-my-zsh
 sudo chmod 744 .zshrc
@@ -18,17 +18,17 @@ echo "Config .zshrc"
 # fi" >> ~/.zshrc
 sudo sed -i "s/robbyrussell/fletcherm/" ~/.zshrc
 sudo sed -i 's/# DISABLE_AUTO_TITLE="true"/DISABLE_AUTO_TITLE="true"/' ~/.zshrc
-sudo sed -i 's/# ENABLE_CORRECTION="true"/ENABLE_CORRECTION="true"/' ~/.zshrc
+sudo sed -i 's/# ENABLE_CORRECTION="false"/ENABLE_CORRECTION="true"/' ~/.zshrc
 
 echo 'HIST_STAMPS="dd.mm.yyyy"' | sudo tee -a ~/.zshrc
 echo "HISTSIZE=10000"  | sudo tee -a ~/.zshrc
 
-echo "NOW=$(date +%Y-%m-%d_%H-%M-%S)" | sudo tee -a ~/.zshrc
-echo "DATE=$(date +%Y-%m-%d)" | sudo tee -a ~/.zshrc
+echo "NOW=\$(date +%Y-%m-%d_%H-%M-%S)" | sudo tee -a ~/.zshrc
+echo "DATE=\$(date +%Y-%m-%d)" | sudo tee -a ~/.zshrc
 
 echo "switch $USER & root terminal to zsh"
 sudo usermod --shell $(which zsh) root
 sudo ln -s /home/$USER/.oh-my-zsh /root/
 sudo ln -s /home/$USER/.zshrc /root/
 
-source ~/.zshrc
+zsh
