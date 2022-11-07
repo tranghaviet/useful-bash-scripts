5c9c89285da347b06ad366e371bdd83049e0320b
Trang Ha Viet <viettrangha@gmail.com>
2022-11-07 22:46:56 +0700

    add crate self signed SSL certificate script

---
+++ linux/ssl/self-signed-ssl-certificate.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Bash Script to create a new self-signed SSL Certificate
+# At the end of creating a new Certificate this script will output a few lines
+# to be copied and placed into NGINX site conf
+#
+# USAGE: this command will ask for the certificate name and number in days it will expire
+# $ mkselfssl
+#
+# OPTIONAL: run the command straightforward
+# $ mkselfssl mycertname 365
+
+# Default dir to place the Certificate
+DIR_SSL_CERT="/etc/nginx/ssl/cert"
+DIR_SSL_KEY="/etc/nginx/ssl/private"
+
+SSLNAME=$1
+SSLDAYS=$2
+
+if [ -z $1 ]; then
+  printf "Enter the SSL Certificate Name:"
+  read SSLNAME
+fi
+
+if [ -z $2 ]; then
+  read -p "How many days the Certificate will be valid [365]: " SSLDAYS
+  name=${SSLDAYS:-365}
+fi
+
+echo "Creating a new Certificate ..."
+openssl req -x509 -nodes -newkey rsa:2048 -keyout $SSLNAME.key -out $SSLNAME.crt -days $SSLDAYS
+
+# Make directory to place SSL Certificate if it doesn't exists
+if [[ ! -d $DIR_SSL_KEY ]]; then
+  sudo mkdir -p $DIR_SSL_KEY
+fi
+
+if [[ ! -d $DIR_SSL_CERT ]]; then
+  sudo mkdir -p $DIR_SSL_CERT
+fi
+
+# Place SSL Certificate within defined path
+sudo mv $SSLNAME.key $DIR_SSL_KEY/$SSLNAME.key
+sudo mv $SSLNAME.crt $DIR_SSL_CERT/$SSLNAME.crt
+
+# Print output for Nginx site config
+printf "+-------------------------------
++ SSL Certificate has been created.
++ Here is the NGINX Config for $SSLNAME
++ Copy it into your nginx config file
++-------------------------------\n\n
+server {
+  listen 80;
+  listen [::]:80;
+  server_name domain.test;
+  return 301 https://$server_name$request_uri;
+}
+
+server {
+  listen 443 ssl http2;
+  listen [::]:443 ssl http2;
+
+  server_name domain.test;
+  root /root-path;
+
+  ssl_protocols TLSv1.2 TLSv1.1 TLSv1;
+  ssl_certificate      $DIR_SSL_CERT/$SSLNAME.crt;
+  ssl_certificate_key  $DIR_SSL_KEY/$SSLNAME.key;
+  ...
+\n\n"
